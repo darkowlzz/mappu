5e51b382f29a8ce06a1344590b0548f25c9fd31e
Sunny <me@darkowlzz.space>
2015-08-09 02:19:54 +0530

    Added service for map.

--- src/model/Person.js
+++ src/model/Person.js
@@ -1,7 +1,10 @@
 class PersonController {
-  constructor($scope, $modal) {
+  constructor($scope, $modal, map) {
     this.scope = $scope;
     this.modal = $modal;
+
+    // Initialize the map.
+    map.init();
 
     this.scope.animationsEnabled = true;
     this.signedIn = false;
@@ -23,20 +26,6 @@ class PersonController {
         that.username = profile.getName();
       });
     }
-
-    let map = L.map('map', {zoomControl: false}).setView([12.9729, 77.5882], 13);
-    /**
-     * ==== mapquest tiles ====
-    L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-        subdomains: ['otile1','otile2','otile3','otile4']
-        }).addTo(map);
-    */
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFya293bHp6IiwiYSI6IjNlNzMyNjAxNzg5MjY3MGRhZDY1OWQ2YzMxMDQ2MzVjIn0.PRWXV8RkEyAtFr7K4dZDgw', {
-      id: 'darkowlzz.94db14bd'
-    }).addTo(map);
-
-    map.addControl(L.control.zoom({position: 'bottomleft'}));
-
   }
 
   signOut() {
@@ -60,6 +49,6 @@ class PersonController {
 
 }
 
-PersonController.$inject = ['$scope', '$modal'];
+PersonController.$inject = ['$scope', '$modal', 'map'];
 
 export { PersonController };
---
+++ src/service/Map.js
@@ -0,0 +1,34 @@
+function MapService () {
+  let map;
+
+  return {
+    // Initialize the map.
+    init: function init () {
+      map = L.map('map', {zoomControl: false}).setView([12.9729, 77.5882], 13);
+      /**
+       * ==== mapquest tiles ====
+      L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+          subdomains: ['otile1','otile2','otile3','otile4']
+          }).addTo(map);
+      */
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZGFya293bHp6IiwiYSI6IjNlNzMyNjAxNzg5MjY3MGRhZDY1OWQ2YzMxMDQ2MzVjIn0.PRWXV8RkEyAtFr7K4dZDgw', {
+        id: 'darkowlzz.94db14bd'
+      }).addTo(map);
+
+      map.addControl(L.control.zoom({position: 'bottomleft'}));
+    },
+
+    // Go to a given latitude and longitude.
+    goto: function goto (lat, lng, zoom = 13) {
+      map.setView([lat, lng], zoom);
+    },
+
+    // Automatically locate.
+    locate: function locate (zoom = 16) {
+      console.log('locating...');
+      map.locate({setView: false, maxZoom: zoom});
+    }
+  }
+}
+
+export { MapService };
