d72025d3f4c3365c3f6a66be696acb3fdff65fa9
Sunny <me@darkowlzz.space>
2015-08-21 06:44:00 +0530

    FB login added.

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -7,8 +7,23 @@ function AuthService ($rootScope, $window, Session, AUTH_EVENTS) {
 
         // Store to sessionStore when not using 3rd party auth
         //$window.sessionStorage['userInfo'] = JSON.stringify({name: profile.getName()});
-        Session.create(profile.getName());
+        Session.create(profile.getName(), 'google');
         $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+      }
+
+      $window.checkLoginState = function checkLoginState() {
+        FB.getLoginStatus(function (response) {
+          if (response.status === 'connected') {
+            FB.api('/me', function (resp) {
+              Session.create(resp.name, 'fb');
+              $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+            });
+          } else if (response.status === 'not_authorized') {
+            console.log('Not logged into the app');
+          } else {
+            console.log('Not logged into fb');
+          }
+        })
       }
     },
 
@@ -24,13 +39,19 @@ function AuthService ($rootScope, $window, Session, AUTH_EVENTS) {
     },
 
     logout: function logout () {
-      let auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(() => {
-        Session.destroy();
-        //$window.sessionStorage.removeItem('userInfo');
-        $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
-      });
-
+      if (Session.loginService == 'google') {
+        let auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(() => {
+          Session.destroy();
+          //$window.sessionStorage.removeItem('userInfo');
+          $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
+        });
+      } else if (Session.loginService == 'fb') {
+        FB.logout(function (response) {
+          Session.destroy();
+          $rootScope.$broadcast(AUTH_EVENTS.logoutSuccess);
+        });
+      }
     }
   }
 }
